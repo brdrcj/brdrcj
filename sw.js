82cdaf7c8965072244d2cab67c0f8b1443b25e2f
l15y <ly444983@live.com>
2022-07-08 10:28:30 +0800

    -

--- sw.js
+++ sw.js
@@ -1,48 +1,50 @@
-const SW_VERSION = '12';
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(self.skipWaiting());
+importScripts(
+    "/static/workbox.js"
+);
+if (workbox) {
+    console.log('workbox加载成功');
+} else {
+    console.log('workbox加载失败');
+}
+
+//设置缓存cachestorage的名称
+workbox.core.setCacheNameDetails({
+    prefix:'yqtj',
+    suffix:'v1'
 });
 
-// 监听 service workers 更新时间
-self.addEventListener('activate', function (event) {
-    // console.log('sw.js 更新');
-    event.waitUntil(
-        Promise.all([
-            self.clients.claim(),
-            // 清理旧版本
-            caches.keys().then(cacheNames => {
-                return Promise.all(
-                    cacheNames.map(cacheName => {
-                        // console.log(cacheName, SW_VERSION);
-                        if (cacheName !== SW_VERSION) {
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
+workbox.skipWaiting();
+workbox.clientsClaim();
+workbox.routing.registerRoute(
+    new RegExp('/'),
+    workbox.strategies.staleWhileRevalidate({
+        //cache名称
+        cacheName: 'lf-sw:static',
+        plugins: [
+            new workbox.expiration.Plugin({
+                //cache最大数量
+                maxEntries: 30
             })
-        ])
-    );
-});
-
-// 缓存请求
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(res => {
-            if (res && res.status == 200 && res.type == 'basic') {
-                console.log("已缓存", res.url);
-                return res;
-            }
-            // 请求是一个流，只能使用一次，为了再次使用这里需要克隆
-            const requestToCache = event.request.clone();
-            // 针对缓存中没有存在资源这里重新请求一下
-            return fetch(requestToCache).then(res => {
-                const responseToCache = res.clone();
-                caches
-                    .open(SW_VERSION)
-                    .then(cache => cache.put(requestToCache, responseToCache));
-                return res;
-            });
-        })
-    );
-});
+        ]
+    })
+);
+workbox.routing.registerRoute(
+    new RegExp('/static/.+'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'lf-sw:img',
+        plugins: [
+            //如果要拿到域外的资源，必须配置
+            //因为跨域使用fetch配置了
+            //mode: 'no-cors',所以status返回值为0，故而需要兼容
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 40,
+                //缓存的时间
+                maxAgeSeconds: 12 * 60 * 60
+            })
+        ]
+    })
+);
